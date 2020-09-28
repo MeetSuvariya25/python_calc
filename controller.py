4abb1a38e3a2856373ffb423fa61ea52f9dda64b
Meet <meetsuvariya@gmail.com>
2020-09-28 14:34:45 +0530

    controller updated

--- controller.py
+++ controller.py
@@ -24,12 +24,4 @@ class Controller:
         expression = self._view.getDisplayText() + sub_exp
         self._view.setDisplayText(expression)
 
-    def _connectSignals(self):
-        """Connect signals and slots."""
-        for btnText, btn in self._view.buttons.items():
-            if btnText not in {'=', 'C'}:
-                btn.clicked.connect(partial(self._buildExpression, btnText))
-
-        self._view.buttons['='].clicked.connect(self._calculateResult)
-        self._view.display.returnPressed.connect(self._calculateResult)
-        self._view.buttons['C'].clicked.connect(self._view.clearDisplay)
+    
